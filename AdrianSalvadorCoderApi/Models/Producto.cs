b98afea720a21cb4accf1c820f5a1a3a5595b615
PinTiTA <85143340+PiNtiT@users.noreply.github.com>
2022-11-29 19:20:23 -0300

    Add files via upload

---
+++ AdrianSalvadorCoderApi/Models/Producto.cs
@@ -0,0 +1,34 @@
+﻿namespace AdrianSalvadorCoderApi.Models
+{
+    public class Producto
+    {
+        public class Producto
+        {
+            public long Id { get; set; }
+            public string Descripciones { get; set; }
+            public double Costo { get; set; }
+            public double PrecioVenta { get; set; }
+            public int Stock { get; set; }
+            public int IdUsuario { get; set; }
+
+            public Producto()
+            {
+                Id = 0;
+                Descripciones = "";
+                Costo = 0;
+                PrecioVenta = 0;
+                Stock = 0;
+            }
+
+            public Producto(long codigo, string descripciones, double costo, double precioVenta, int stock, int idUsuario)
+            {
+                Id = codigo;
+                Descripciones = descripciones;
+                Costo = costo;
+                PrecioVenta = precioVenta;
+                Stock = stock;
+                IdUsuario = idUsuario;
+            }
+        }
+    }
+}
---
+++ AdrianSalvadorCoderApi/Repositories/ProductoRepositorio.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.SqlClient;
+using AdrianSalvadorCoderApi.Models;
+using System.Data;
+
+namespace AdrianSalvadorCoderApi.Repositories
+{
+    public class ProductoRepositorio
+    {
+        private SqlConnection? conexion;
+        private String CadenaConexion = "Server=sql.bsite.net\\MSSQL2016;" +
+            "Database=adrians_E001;" +
+            "User Id=adrians_E001;" +
+            "Password=AsdaF1159*;";
+    }
+    public ProductoRepositorio()
+    {
+        try
+        {
+            conexion = new SqlConnection(CadenaConexion);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    public List<Producto> listarProductos()
+    {
+        List<Producto> listaProductos = new List<Producto>();
+        if (conexion == null)
+        {
+            throw new Exception("Conexión no establecida");
+        }
+        try
+        {
+            using (SqlCommand command = new SqlCommand("SELECT * FROM producto", conexion))
+            {
+                conexion.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Producto producto = new Producto();
+                            producto.Id = Convert.ToInt32(reader["Id"].ToString());
+                            producto.Descripciones = reader["Descripciones"].ToString();
+                            producto.Costo = Convert.ToDecimal(reader["Costo"].ToString());
+                            producto.PrecioVenta = Convert.ToDecimal(reader["PrecioVenta"].ToString());
+                            producto.Stock = Convert.ToInt32(reader["Stock"].ToString());
+                            listaProductos.Add(producto);
+                        }
+                    }
+                }
+            }
+            conexion.Close();
+        }
+        catch(Exception)
+        {
+            throw;
+        }
+        return listaProductos;
+    }
+
+}
