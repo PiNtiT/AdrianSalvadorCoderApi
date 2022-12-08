e20392b8113ce864d9f7a46442ee6e9051d853a0
PinTiTA <85143340+PiNtiT@users.noreply.github.com>
2022-12-07 22:51:31 -0300

    Update ProductoRepositorio.cs

--- AdrianSalvadorCoderApi/Repositories/ProductoRepositorio.cs
+++ AdrianSalvadorCoderApi/Repositories/ProductoRepositorio.cs
@@ -62,6 +62,173 @@ namespace AdrianSalvadorCoderApi.Repositories
             }
             return listaProductos;
         }
+        
+        
+        public void crearProducto(Producto producto)  //  Para crear nuevo producto
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Producto(Descripciones, Costo, PrecioVenta, Stock, IdUsuario) VALUES(@descripcion, @costo, @precioVenta, @stock, @idUsuario)", conexion)) //comando de consulta sql
+                {
+                    conexion.Open(); 
+                    cmd.Parameters.Add(new SqlParameter("descripcion", SqlDbType.VarChar) { Value = producto.Descripciones });
+                    cmd.Parameters.Add(new SqlParameter("costo", SqlDbType.Float) { Value = producto.Costo });
+                    cmd.Parameters.Add(new SqlParameter("precioVenta", SqlDbType.Float) { Value = producto.PrecioVenta });
+                    cmd.Parameters.Add(new SqlParameter("stock", SqlDbType.Int) { Value = producto.Stock });
+                    cmd.Parameters.Add(new SqlParameter("idUsuario", SqlDbType.BigInt) { Value = producto.IdUsuario });
+                    cmd.ExecuteNonQuery();
+
+                    conexion.Close(); 
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        public bool eliminarProducto(long id)  //  Eliminar producto por id
+        {
+
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+                int filasAfectadas;
+                using (SqlCommand cmd = new SqlCommand("DELETE FROM producto WHERE id=@id", conexion)) //  Borrar por sql
+                {
+                    conexion.Open(); 
+                    cmd.Parameters.Add(new SqlParameter("id", SqlDbType.BigInt) { Value = id });
+                    filasAfectadas = cmd.ExecuteNonQuery();
+                    conexion.Close(); 
+                    return filasAfectadas > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
+
+        private Producto obtenerProductoDesdeReader(SqlDataReader reader)
+        {
+            Producto producto = new Producto();
+            producto.Id = (int)Convert.ToInt64(reader["Id"]);
+            producto.Descripciones = reader["Descripciones"].ToString();
+            producto.PrecioVenta = (decimal)Convert.ToDouble(reader["PrecioVenta"].ToString());
+            producto.Costo = (decimal)Convert.ToDouble(reader["Costo"].ToString());
+            producto.Stock = int.Parse(reader["Stock"].ToString());
+            producto.IdUsuario = (int)Convert.ToInt64(reader["IdUsuario"]);
+            return producto;
+
+        }
+
+        public Producto? obtenerProducto(long id)  // Traer producto por id
+        {
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Producto WHERE id=@id", conexion)) //  Consulta sql
+                {
+                    conexion.Open(); 
+                    cmd.Parameters.Add(new SqlParameter("id", SqlDbType.BigInt) { Value = id });
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            Producto producto = obtenerProductoDesdeReader(reader);
+                            return producto;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                conexion.Close(); 
+            }
+
+        }
+
+        public Producto? actualizarProducto(long id, Producto productoParaAactualizar)
+        {
+            if (conexion == null)
+            {
+                throw new Exception("Conexion no establecida");
+            }
+            try
+            {
+                Producto? producto = obtenerProducto(id);
+                if (producto == null)
+                {
+                    return null;
+                }
+                List<string> camposAActulizar = new List<string>();
+                if (producto.Descripciones != productoParaAactualizar.Descripciones && !string.IsNullOrEmpty(productoParaAactualizar.Descripciones))
+                {
+                    camposAActulizar.Add("Descripciones = @descripcion");
+                }
+                if (producto.Costo != productoParaAactualizar.Costo && productoParaAactualizar.Costo > 0)
+                {
+                    camposAActulizar.Add("Costo = @costo");
+                }
+                if (producto.PrecioVenta != productoParaAactualizar.PrecioVenta && productoParaAactualizar.PrecioVenta > 0)
+                {
+                    camposAActulizar.Add("PrecioVenta = @precioVenta");
+                }
+                if (producto.Stock != productoParaAactualizar.Stock && productoParaAactualizar.Stock > 0)
+                {
+                    camposAActulizar.Add("Stock = @stock");
+                }
+                if (producto.IdUsuario != productoParaAactualizar.IdUsuario && productoParaAactualizar.IdUsuario > 0)
+                {
+                    camposAActulizar.Add("IdUsuario = @idUsuario");
+                }
+                if (camposAActulizar.Count == 0)
+                {
+                    throw new Exception("No hay nada para actualizar");
+                }
+                using (SqlCommand cmd = new SqlCommand($"UPDATE Producto SET {String.Join(", ", camposAActulizar)} WHERE id=@id", conexion))
+                {
+                    conexion.Open(); //abro la conexion con la base de datos
+                    cmd.Parameters.Add(new SqlParameter("descripcion", SqlDbType.VarChar) { Value = productoParaAactualizar.Descripciones });
+                    cmd.Parameters.Add(new SqlParameter("costo", SqlDbType.Float) { Value = productoParaAactualizar.Costo });
+                    cmd.Parameters.Add(new SqlParameter("precioVenta", SqlDbType.Float) { Value = productoParaAactualizar.PrecioVenta });
+                    cmd.Parameters.Add(new SqlParameter("stock", SqlDbType.Int) { Value = productoParaAactualizar.Stock });
+                    cmd.Parameters.Add(new SqlParameter("idUsuario", SqlDbType.BigInt) { Value = productoParaAactualizar.IdUsuario });
+                    cmd.Parameters.Add(new SqlParameter("id", SqlDbType.BigInt) { Value = id });
+                    cmd.ExecuteNonQuery();
+                    return productoParaAactualizar;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+        }
     }
 
 
